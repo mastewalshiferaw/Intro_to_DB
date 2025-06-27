34cf71e32c099650a016fc8790d68277309c7ded
Mastewal <mastewalgshiferaw@gmail.com>
2025-06-28 02:29:07 +0300

    committed task 1

---
+++ MySQLServer.py
@@ -0,0 +1,9 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+  with mysql.connector.connect(
+    host = 'localhost'
+    user = 'Mastawal'
+    password = '20wasGod25te' 
+    )
