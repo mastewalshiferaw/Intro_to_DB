bff301f37d6db0564acb3cd946d3057072c1851f
Mastewal <mastewalgshiferaw@gmail.com>
2025-06-27 20:45:07 +0300

    Committed Task 0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE alx_book_store
-USE alx_book_store
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
 CREATE TABLE Books (book_id INT PRIMARY KEY, title VARCHAR (130), author_id INT, FOREIGN KEY (author_id) REFERENCES Authors(author_id), price DOUBLE, publication_date DATE);
 
 CREATE TABLE Authors (author_id INT PRIMARY KEY, author_name VARCHAR (215))
