f1e7683a1cb8bd18dc79a88c5bc143406195688c
Mastewal <mastewalgshiferaw@gmail.com>
2025-06-28 19:19:43 +0300

    Committed Task 4

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,11 @@
-USE alx_book_store;
 SELECT
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
 FROM
     INFORMATION_SCHEMA.COLUMNS
 WHERE
-    TABLE_SCHEMA = "alx_book_store" AND TABLE_NAME = 'Books';
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
